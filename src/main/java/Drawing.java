ceb3d340a4bd8b40d006a8a6fb68fd970f1b106f
nc619 <nc619@ic.ac.uk>
2021-10-25 19:34:34 +0100

    Initial commit

---
+++ src/main/java/Drawing.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Drawing extends Canvas {
+    // A private field called f of class Frame. This is also in the AWT.
+
+    Point p=new Point(200,200);
+    Color c=new Color(0x992266);
+    int rad=50;
+    private Circle circle1 = new Circle(p, c, 50);
+    private Frame f;
+
+    // The constructor
+    public Drawing() {
+
+        f = new Frame("My window"); // Instantiates the Frame
+        f.add(this); // Adds the Canvas to the Frame
+        f.setLayout(null); // Stops the frame from trying to layout contents
+        f.setSize(400, 400); // Sets the dimensions of the frame
+        f.setVisible(true);
+        f.addWindowListener(new WindowAdapter() { // Closes the program if close window clicked
+            public void windowClosing(WindowEvent e) {
+                f.dispose();
+            }
+        });
+        setBackground(Color.WHITE); // Sets the Canvas background
+        setSize(400, 400); // Sets the Canvas size to be the same as the frame
+    }
+
+    public void paint(Graphics g)
+    {
+        circle1.draw(g);
+    }
+}
